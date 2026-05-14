1591757829cb127478c9005e9e37c685a35f6172
DiegoMor24 <diegovenancio2005@gmail.com>
2026-05-14 07:57:07 -0300

    Configure Vercel Node API routes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 import { handleRequest } from "../index.js";
 
 export default function handler(request, response) {
+  if (request.url?.startsWith("/api/index.js?")) {
+    const url = new URL(request.url, `http://${request.headers.host}`);
+    const path = url.searchParams.get("path") || "";
+    request.url = `/api/${path}`;
+  }
   return handleRequest(request, response);
 }
